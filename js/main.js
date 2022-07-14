a9651eb3871a5ef5166edd36e818fa6d05d9e3df
vtnboolean <vnthuanx@gmail.com>
2022-07-14 02:43:26 +0200

    changed type of var

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ const app = new Vue(
     },
 
     methods: {
-        
+
     },
 
     created() {
         // Riempi la lista email
-        for (var i = 0; i < 10; i++) {
+        for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => {
                 this.emails.push(response.data.response);
